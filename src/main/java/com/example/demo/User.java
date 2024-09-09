eff435dffc2c2d0bc4bfd1838ef0e5acaa3dc7c7
Nihal <Nihal@NEHAL>
2024-09-09 13:52:09 +0530

    register page changes

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+
+public class User {
+	private String mailId;
+	private String password;
+	private String fName;
+	private String lName;
+	private String role;
+	private int id;
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
